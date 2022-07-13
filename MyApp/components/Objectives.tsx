58283483d4cf6076c562f4ac3baf70574fb1de36
oguzhansenokur <oguzhansenokur61@gmail.com>
2022-07-13 23:59:14 +0300

    Edit Screen Has been Re-Designed. Plus Screen Has Been Added. Minus Screen Has Been Added.

--- MyApp/components/Objectives.tsx
+++ MyApp/components/Objectives.tsx
@@ -1,17 +1,17 @@
-import { Image, StyleSheet, Text, Touchable, TouchableOpacity, View,Alert,Modal } from 'react-native'
-import React, { useState ,useEffect} from 'react'
+import { Image, StyleSheet, Text, Touchable, TouchableOpacity, View, Alert, Modal, TextInput } from 'react-native'
+import React, { useState, useEffect } from 'react'
 import ProgressBar from './ProgressBar'
 import AsyncStorage from '@react-native-async-storage/async-storage';
-
+import { BottomSheet } from 'react-native-btr';
 interface Props {
     id: number,
     objectName: String,
-    objectAmount:number,
-    objectCurr : number,
-    objectNote : String,
+    objectAmount: number,
+    objectCurr: number,
+    objectNote: String,
 }
 
-const AddWallet=()=> {
+const AddWallet = () => {
 
 
 }
@@ -21,180 +21,309 @@ const AddWallet=()=> {
 
 
 
-export default function Objectives(props:Props) {
-    const [progNum,setProgNum]=useState(props.objectAmount);
-    const [progColor,setProgColor]=useState('#000');
-    const [modalVisible,setModalVisible]=useState(false);
+export default function Objectives(props: Props) {
+    const [progNum, setProgNum] = useState(props.objectAmount);
+    const [progColor, setProgColor] = useState('#000');
+    const [modalVisible, setModalVisible] = useState(false);
+    const [minusVisible, setMinusVisible] = useState(false);
+    const [plusVisible, setPlusVisible] = useState(false);
 
-    const checkNums = () =>{
-        if(progNum>0 && progNum<25)
-        {
+    const checkNums = () => {
+        if (progNum > 0 && progNum < 25) {
             setProgColor('#E98080')
         }
-        else if(progNum>24 && progNum<75)
-        {
+        else if (progNum > 24 && progNum < 75) {
             setProgColor('#F6FA55')
         }
-        else if (progNum>74 && progNum<100)
-        {
+        else if (progNum > 74 && progNum < 100) {
             setProgColor('#1DF667')
         }
-        else if(progNum==100)
-        {
+        else if (progNum == 100) {
             setProgColor('#158D3E')
 
         }
     }
-    const calculateProg=()=>{
-        if(props.objectAmount>0 && props.objectCurr>0 && props.objectAmount>=props.objectCurr)
-        {
-            setProgNum(Math.round((props.objectCurr/props.objectAmount)*100))
+    const calculateProg = () => {
+        if (props.objectAmount > 0 && props.objectCurr > 0 && props.objectAmount >= props.objectCurr) {
+            setProgNum(Math.round((props.objectCurr / props.objectAmount) * 100))
             checkNums()
         }
-      
+
     }
 
-   
 
- 
-    useEffect(()=>{
+
+
+    useEffect(() => {
         calculateProg()
         checkNums()
-    },[])
+    }, [])
 
-    const toggleModalEdit=()=>{
+    const toggleModalEdit = () => {
         setModalVisible(!modalVisible)
     }
+    const toggleMinus = () => {
+        setMinusVisible(!minusVisible)
+    }
+    const togglePlus = () => {
+        setPlusVisible(!plusVisible)
+    }
 
-   
-    const deleteItem= async ()=>{
-       const result= await AsyncStorage.getItem('objective')
-       let objectives=[]
-       let objectives2=[]
-       if(result!==null)
-       {objectives=JSON.parse(result)
-        console.log(objectives)
-    } 
-        objectives2=objectives.filter(item=>item.id!=props.id)
-        await AsyncStorage.setItem('objective',JSON.stringify(objectives2))
-    
+    const deleteItem = async () => {
+        const result = await AsyncStorage.getItem('objective')
+        let objectives = []
+        let objectives2 = []
+        if (result !== null) {
+            objectives = JSON.parse(result)
+            console.log(objectives)
+        }
+        objectives2 = objectives.filter(item => item.id != props.id)
+        await AsyncStorage.setItem('objective', JSON.stringify(objectives2))
+
 
     }
 
-    const DeleteItemAlert=()=>{
-        Alert.alert('Gerçekten Hedefinizi Silmek İstiyor musunuz?','Bu işlem geri alınamaz.',[{text:'Sil',onPress:deleteItem},{text:'İptal',style:'cancel'}],{cancelable:true})
-    
-    }
-  
+    const DeleteItemAlert = () => {
+        Alert.alert('Gerçekten Hedefinizi Silmek İstiyor musunuz?', 'Bu işlem geri alınamaz.', [{ text: 'Sil', onPress: deleteItem }, { text: 'İptal', style: 'cancel' }], { cancelable: true })
 
-  return (
-    <View style={styles.objectiveDiv} >
-        <View style={styles.titleDiv} ><Text style={styles.titleText}>{props.objectName}</Text></View>
-        <View style={styles.contentDiv} ><ProgressBar objectAmount={props.objectAmount} objectCurr={props.objectCurr} progColor={progColor} progNum={progNum} />
-        
-        </View>
-      <View style={styles.buttonsDiv} ><TouchableOpacity style={styles.addButton}><Image style={{alignSelf:'center'}} source={require('./icons/plus.png')} /></TouchableOpacity><TouchableOpacity style={styles.addButton}><Image style={{alignSelf:'center'}} source={require('./icons/minus.png')} /></TouchableOpacity><TouchableOpacity onPress={DeleteItemAlert} style={styles.deleteButon}><Image style={{alignSelf:'center'}} source={require('./icons/close.png')} /></TouchableOpacity><TouchableOpacity  onPress={toggleModalEdit} style={styles.editButton}><Image style={{alignSelf:'center'}} source={require('./icons/edit.png')} /></TouchableOpacity></View>
-      <Modal animationType='slide'
-                transparent={true}
-                visible={modalVisible}
-                onRequestClose={toggleModalEdit}
-        >
-            <View style={styles.editModalDiv} >
-                
-                <View style={styles.editModalDivInner} >
-                <TouchableOpacity style={styles.closeButton}  onPress={toggleModalEdit}>
-                    <Image source={require('./icons/close.png')} />
-               </TouchableOpacity>
+    }
+
+
+    return (
+        <View style={styles.objectiveDiv} >
+            <View style={styles.titleDiv} ><Text style={styles.titleText}>{props.objectName}</Text></View>
+            <View style={styles.contentDiv} ><ProgressBar objectAmount={props.objectAmount} objectCurr={props.objectCurr} progColor={progColor} progNum={progNum} />
+
+            </View>
+            <View style={styles.buttonsDiv} ><TouchableOpacity onPress={togglePlus} style={styles.addButton}><Image style={{ alignSelf: 'center' }} source={require('./icons/plus.png')} /></TouchableOpacity><TouchableOpacity onPress={toggleMinus} style={styles.addButton}><Image style={{ alignSelf: 'center' }} source={require('./icons/minus.png')} /></TouchableOpacity><TouchableOpacity onPress={DeleteItemAlert} style={styles.deleteButon}><Image style={{ alignSelf: 'center' }} source={require('./icons/close.png')} /></TouchableOpacity><TouchableOpacity onPress={toggleModalEdit} style={styles.editButton}><Image style={{ alignSelf: 'center' }} source={require('./icons/edit.png')} /></TouchableOpacity></View>
+
+
+            <BottomSheet visible={modalVisible}
+                onBackButtonPress={toggleModalEdit}
+                onBackdropPress={toggleModalEdit}>
+                <View style={styles.bottomNavigationView} >
+                    <View style={styles.headerInput} ><Text style={{ fontSize: 25, color: '#7B3B75', fontWeight: 'bold' }}>Hedef Düzenleme Ekranı</Text></View>
+                    <View style={styles.formDiv} >
+                        <View style={styles.itemDiv} >
+                            <Text style={{ color: '#7B3B75', fontWeight: 'bold', fontSize: 16, marginLeft: '12%' }}>Hedef Adı</Text>
+                            <TextInput style={{ backgroundColor: '#F5F5F5', width: '90%', height: 35, borderRadius: 50, paddingLeft: 15, color: 'black', fontWeight: 'bold', alignSelf: 'center', marginTop: 5, marginBottom: 15 }} />
+                            <Text style={{ color: '#7B3B75', fontWeight: 'bold', fontSize: 16, marginLeft: '12%' }}>Hedef Maaliyeti</Text>
+                            <TextInput style={{ backgroundColor: '#F5F5F5', width: '90%', height: 35, borderRadius: 50, paddingLeft: 15, color: 'black', fontWeight: 'bold', alignSelf: 'center', marginTop: 5, marginBottom: 15 }} />
+                            <Text style={{ color: '#7B3B75', fontWeight: 'bold', fontSize: 16, marginLeft: '12%' }}>Mevcut Maaliyet</Text>
+                            <TextInput style={{ backgroundColor: '#F5F5F5', width: '90%', height: 35, borderRadius: 50, paddingLeft: 15, color: 'black', fontWeight: 'bold', alignSelf: 'center', marginTop: 5, marginBottom: 15 }} />
+                            <Text style={{ color: '#7B3B75', fontWeight: 'bold', fontSize: 16, marginLeft: '12%' }}>Hedef Notu</Text>
+                            <TextInput style={{ backgroundColor: '#F5F5F5', width: '90%', height: 35, borderRadius: 50, paddingLeft: 15, color: 'black', fontWeight: 'bold', alignSelf: 'center', marginTop: 5, marginBottom: 15 }} />
+                        </View>
+                        <View style={{ flexDirection: 'column', justifyContent: 'center', width: '50%', height: '2%' }}>
+
+
+
+                            <TouchableOpacity
+                                style={{ backgroundColor: '#7C3E66', width: '100%', height: '75%', margin: 85, justifyContent: 'center', alignItems: 'center', borderRadius: 50, }}
+                                activeOpacity={1}
+
+
+                            >
+                                <Text style={{ fontSize: 25, color: 'white', fontWeight: 'bold' }}>Düzenle</Text>
+                            </TouchableOpacity>
+                        </View>
+
+                    </View>
 
                 </View>
-            </View>
-        
-        </Modal>
-    </View>
-  )
+            </BottomSheet>
+            <BottomSheet visible={minusVisible}
+                onBackButtonPress={toggleMinus}
+                onBackdropPress={toggleMinus}>
+                <View style={styles.bottomNavigationView} >
+                    <View style={styles.headerInput} ><Text style={{ fontSize: 25, color: '#7B3B75', fontWeight: 'bold' }}>Para Çıkışı Ekranı</Text></View>
+                    <View style={styles.formDiv} >
+                        <View style={styles.itemDiv} >
+
+                            <Text style={{ color: '#7B3B75', fontWeight: 'bold', fontSize: 16, marginLeft: '12%' }}>Eksilecek Para </Text>
+                            <TextInput style={{ backgroundColor: '#F5F5F5', width: '90%', height: 35, borderRadius: 50, paddingLeft: 15, color: 'black', fontWeight: 'bold', alignSelf: 'center', marginTop: 5, marginBottom: 15 }} />
+
+                        </View>
+                        <View style={{ flexDirection: 'column', justifyContent: 'center', width: '50%', height: '2%' }}>
+
+
+
+                            <TouchableOpacity
+                                style={{ backgroundColor: '#7C3E66', width: '100%', height: '75%', margin: 85, justifyContent: 'center', alignItems: 'center', borderRadius: 50, }}
+                                activeOpacity={1}
+
+
+                            >
+                                <Text style={{ fontSize: 25, color: 'white', fontWeight: 'bold' }}>Devam Et</Text>
+                            </TouchableOpacity>
+                        </View>
+
+                    </View>
+
+                </View>
+            </BottomSheet>
+            <BottomSheet visible={plusVisible}
+                onBackButtonPress={togglePlus}
+                onBackdropPress={togglePlus}>
+                <View style={styles.bottomNavigationView} >
+                    <View style={styles.headerInput} ><Text style={{ fontSize: 25, color: '#7B3B75', fontWeight: 'bold' }}>Para Girişi Ekranı</Text></View>
+                    <View style={styles.formDiv} >
+                        <View style={styles.itemDiv} >
+
+                            <Text style={{ color: '#7B3B75', fontWeight: 'bold', fontSize: 16, marginLeft: '12%' }}>Eklenecek Para </Text>
+                            <TextInput style={{ backgroundColor: '#F5F5F5', width: '90%', height: 35, borderRadius: 50, paddingLeft: 15, color: 'black', fontWeight: 'bold', alignSelf: 'center', marginTop: 5, marginBottom: 15 }} />
+
+                        </View>
+                        <View style={{ flexDirection: 'column', justifyContent: 'center', width: '50%', height: '2%' }}>
+
+
+
+                            <TouchableOpacity
+                                style={{ backgroundColor: '#7C3E66', width: '100%', height: '75%', margin: 85, justifyContent: 'center', alignItems: 'center', borderRadius: 50, }}
+                                activeOpacity={1}
+
+
+                            >
+                                <Text style={{ fontSize: 25, color: 'white', fontWeight: 'bold' }}>Devam Et</Text>
+                            </TouchableOpacity>
+                        </View>
+
+                    </View>
+
+                </View>
+            </BottomSheet>
+
+
+
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({
     objectiveDiv:
     {
-        backgroundColor:'#fff',
-        margin:20,
-        height:175,
-        borderRadius:25,
-        shadowOpacity:1,
-        shadowColor:'black'
+        backgroundColor: '#fff',
+        margin: 20,
+        height: 175,
+        borderRadius: 25,
+        shadowOpacity: 1,
+        shadowColor: 'black'
 
-        
-    },
-    titleDiv:{
-        padding:10,
-        justifyContent:'center',
-    },
-    titleText:{
-        fontWeight:'bold',
-        textAlign:'center',
-        color:'black',
-        fontSize:17
 
     },
-    buttonsDiv:{
-        flexDirection:'row',
-        justifyContent:'space-between',
-        padding:10,
+    titleDiv: {
+        padding: 10,
+        justifyContent: 'center',
+    },
+    titleText: {
+        fontWeight: 'bold',
+        textAlign: 'center',
+        color: 'black',
+        fontSize: 17
 
-        borderBottomLeftRadius:25,
-        borderBottomRightRadius:25,
     },
-    contentDiv:{
-        margin:20
+    buttonsDiv: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        padding: 10,
+
+        borderBottomLeftRadius: 25,
+        borderBottomRightRadius: 25,
     },
-    addButton:{
-        borderRadius:100,
-        backgroundColor:'#B46EAD',
-        width:'20%',
-        height:30,
-        justifyContent:'center'
-        
-        
+    contentDiv: {
+        margin: 20
     },
-    deleteButon:{
-        borderRadius:100,
-        backgroundColor:'#B92525',
-        width:'20%',
-        height:30,
-        justifyContent:'center'
+    addButton: {
+        borderRadius: 100,
+        backgroundColor: '#B46EAD',
+        width: '20%',
+        height: 30,
+        justifyContent: 'center'
+
+
     },
-    editButton:{
-        borderRadius:100,
-        backgroundColor:'#CCAB35',
-        width:'20%',
-        height:30,
-        justifyContent:'center'
+    deleteButon: {
+        borderRadius: 100,
+        backgroundColor: '#B92525',
+        width: '20%',
+        height: 30,
+        justifyContent: 'center'
+    },
+    editButton: {
+        borderRadius: 100,
+        backgroundColor: '#CCAB35',
+        width: '20%',
+        height: 30,
+        justifyContent: 'center'
     }
     ,
-    editModalDiv:{
-        backgroundColor:'gray',
-        height:'100%',
-        marginTop:'24%',
-        opacity:0.9,
-        justifyContent:'center',
-        zIndex:-1,
+    editModalDiv: {
+        backgroundColor: 'gray',
+        height: '100%',
+        marginTop: '24%',
+        opacity: 0.9,
+        justifyContent: 'center',
+        elevation: -1,
     },
-    editModalDivInner:{
-        backgroundColor:'white',
-        margin:20,
-        width:'100%',
-        height:'60%',
-        alignSelf:'center',
-        zIndex:1,
+    editModalDivInner: {
+        backgroundColor: 'white',
+        marginTop: '75%',
+        width: '100%',
+        height: '75%',
+        alignSelf: 'center',
+        elevation: 3,
+
+    }, bottomNavigationView: {
+        backgroundColor: '#fff',
+        width: '100%',
+        height: 500,
+        alignItems: 'center',
+    },
+    closeButton: {
+        backgroundColor: '#B92525',
+        width: '6%',
+        height: '6%',
+        borderRadius: 100,
+        alignSelf: 'flex-end',
+        margin: 10
+    },
+    editTitle: {
+        fontWeight: 'bold',
+        textAlign: 'center',
+        fontSize: 20,
+        color: '#7B3B75',
+    },
+    editForm: {
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignSelf: 'center',
+
 
     },
-    closeButton:{
-        backgroundColor:'#B92525',
-        width:'6%',
-        height:'6%',
-        borderRadius:100,
-        alignSelf:'flex-end',
-        margin:10
+    textInput:
+        { width: 300, height: '20%', borderColor: 'black', backgroundColor: '#F5F5F5', borderWidth: 1, marginBottom: 20, borderRadius: 50 },
+    titleLabelDiv:
+    {
+        justifyContent: 'center',
+        alignSelf: 'center',
+        marginTop: '10%',
+        marginBottom: '5%',
+    },
+    formDiv: {
+        width: '100%',
+        height: '800%',
+        padding: 20
+    },
+    itemDiv: {
+        flexDirection: 'column',
+        justifyContent: 'center',
+        width: '100%',
+    }, headerInput: {
+        width: '100%',
+        height: '10%',
+        alignItems: 'center',
+        justifyContent: 'center'
+
+
     }
 })
